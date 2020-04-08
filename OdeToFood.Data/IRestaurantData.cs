fada939bcb2fba9d4a2ce3ede7271d77c8c80598
fabioono25 <fabioono25@gmail.com>
2020-04-08 22:53:10 +0100

    Add and expose data

---
+++ OdeToFood.Data/IRestaurantData.cs
@@ -0,0 +1,33 @@
+﻿using OdeToFood.Core;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OdeToFood.Data
+{
+    public interface IRestaurantData
+    {
+        IEnumerable<Restaurant> GetAll();
+    }
+
+    public class InMemoryRestaurantData : IRestaurantData
+    {
+        //a List is not thread safe: cannot process multiple requests
+        List<Restaurant> restaurants;
+
+        public InMemoryRestaurantData()
+        {
+            restaurants = new List<Restaurant>
+            {
+                new Restaurant { Id = 1, Name = "Japan Food", Cuisine = Restaurant.CuisineType.Japanese, Location = "Jp Street" },
+                new Restaurant { Id = 2, Name = "Italian Food", Cuisine = Restaurant.CuisineType.Italian, Location = "Ita Street" },
+                new Restaurant { Id = 3, Name = "Indian Food", Cuisine = Restaurant.CuisineType.Indian, Location = "Indian Street" }
+            };
+        }
+
+        public IEnumerable<Restaurant> GetAll()
+        {
+            return restaurants;
+        }
+    }
+}
