755a196afbefff7fd2c79b0bc755f6d491a71d9f
fabioono25 <fabioono25@gmail.com>
2020-04-11 17:51:12 +0100

    implemente the delete method

--- OdeToFood.Data/IRestaurantData.cs
+++ OdeToFood.Data/IRestaurantData.cs
@@ -1,6 +1,5 @@
 ﻿using OdeToFood.Core;
 using System.Collections.Generic;
-using System.Linq;
 
 namespace OdeToFood.Data
 {
@@ -10,62 +9,7 @@ namespace OdeToFood.Data
         Restaurant GetById(int id);
         Restaurant Update(Restaurant updatedRestaurant);
         Restaurant Add(Restaurant newRestaurant);
+        Restaurant Delete(int id);
         int Commit();
-    }
-
-    public class InMemoryRestaurantData : IRestaurantData
-    {
-        //a List is not thread safe: cannot process multiple requests
-        List<Restaurant> restaurants;
-
-        public InMemoryRestaurantData()
-        {
-            restaurants = new List<Restaurant>
-            {
-                new Restaurant { Id = 1, Name = "Japan Food", Cuisine = CuisineType.Japanese, Location = "Jp Street" },
-                new Restaurant { Id = 2, Name = "Italian Food", Cuisine = CuisineType.Italian, Location = "Ita Street" },
-                new Restaurant { Id = 3, Name = "Indian Food", Cuisine = CuisineType.Indian, Location = "Indian Street" }
-            };
-        }
-
-        //Units of work
-        public int Commit()
-        {
-            return 0;
-        }
-
-        public Restaurant GetById(int id)
-        {
-            return restaurants.SingleOrDefault(r => r.Id == id);
-        }
-
-        public IEnumerable<Restaurant> GetRestaurantsByName(string name)
-        {
-            return from r in restaurants
-                   where string.IsNullOrEmpty(name) || r.Name.StartsWith(name)
-                   orderby r.Name
-                   select r;
-        }
-
-        public Restaurant Add(Restaurant newRestaurant)
-        {
-            restaurants.Add(newRestaurant);
-            newRestaurant.Id = restaurants.Max(r => r.Id) + 1;
-            return newRestaurant;
-        }
-        
-        public Restaurant Update(Restaurant updatedRestaurant)
-        {
-            var restaurant = restaurants.SingleOrDefault(r => r.Id == updatedRestaurant.Id);
-
-            if (restaurant != null)
-            {
-                restaurant.Name = updatedRestaurant.Name;
-                restaurant.Location = updatedRestaurant.Location;
-                restaurant.Cuisine = updatedRestaurant.Cuisine;
-            }
-
-            return restaurant;
-        }
     }
 }
