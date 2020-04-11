86ebd3e91ea0fb8039e6fafbd50690896fe79f0f
fabioono25 <fabioono25@gmail.com>
2020-04-11 18:00:17 +0100

    Add SqlRestaurantData

---
+++ OdeToFood.Data/SqlRestaurantData.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OdeToFood.Core;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace OdeToFood.Data
+{
+    public class SqlRestaurantData : IRestaurantData
+    {
+        private readonly OdeToFoodDbContext context;
+
+        public SqlRestaurantData(OdeToFoodDbContext context)
+        {
+            this.context = context;
+        }
+
+        public Restaurant Add(Restaurant newRestaurant)
+        {
+            //context.Restaurants.Add()
+            context.Add(newRestaurant);
+            return newRestaurant;
+        }
+
+        public int Commit()
+        {
+            return context.SaveChanges();
+        }
+
+        public Restaurant Delete(int id)
+        {
+            var restaurant = GetById(id);
+
+            if (restaurant != null)
+            {
+                context.Restaurants.Remove(restaurant);
+            }
+
+            return restaurant;
+        }
+
+        public Restaurant GetById(int id)
+        {
+            return context.Restaurants.Find(id);
+        }
+
+        public IEnumerable<Restaurant> GetRestaurantsByName(string name)
+        {
+            var query = from r in context.Restaurants
+                        where r.Name.StartsWith(name) || string.IsNullOrEmpty(name)
+                        orderby r.Name
+                        select r;
+
+            return query;
+        }
+
+        public Restaurant Update(Restaurant updatedRestaurant)
+        {
+            var entity = context.Restaurants.Attach(updatedRestaurant); //important
+            entity.State = EntityState.Modified;
+            return updatedRestaurant;
+        }
+    }
+}
