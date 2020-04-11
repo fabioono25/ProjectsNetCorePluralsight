c5ddc7b93119a48f0a9f4bed91a7153350c38cec
fabioono25 <fabioono25@gmail.com>
2020-04-11 19:54:43 +0100

    add controllers to return restaurants

--- OdeToFood/Startup.cs
+++ OdeToFood/Startup.cs
@@ -38,6 +38,7 @@ namespace OdeToFood
             services.AddRazorPages().AddRazorRuntimeCompilation();
 
             services.AddRazorPages();
+            services.AddControllers();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -65,6 +66,7 @@ namespace OdeToFood
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapRazorPages();
+                endpoints.MapControllers();
             });
         }
     }
