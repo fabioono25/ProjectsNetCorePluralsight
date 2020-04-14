fa6facc6ff7898c00745803b205f0d9834748d7a
fabioono25 <fabioono25@gmail.com>
2020-04-14 22:09:50 +0100

    Creating the domain layer

---
+++ PieShop/Models/IPieRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PieShop.Models
+{
+    public interface IPieRepository
+    {
+        IEnumerable<Pie> Pies { get; }
+        IEnumerable<Pie> PiesOfTheWeek { get; }
+
+        Pie GetPieById(int pieId);
+
+        void CreatePie(Pie pie);
+
+        void UpdatePie(Pie pie);
+    }
+}
--- PieShop/Startup.cs
+++ PieShop/Startup.cs
@@ -1,12 +1,8 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using PieShop.Models;
 
 namespace PieShop
 {
@@ -16,6 +12,10 @@ namespace PieShop
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            //register services
+            services.AddScoped<IPieRepository, MockPieRepository>();
+            services.AddScoped<ICategoryRepository, MockCategoryRepository>();
+
             //support for MVC
             services.AddControllersWithViews();
         }
