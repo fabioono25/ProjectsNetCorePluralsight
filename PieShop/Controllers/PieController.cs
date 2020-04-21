da665c0ae7d8f86593847a893601bd94d11a6cbe
fabioono25 <fabioono25@gmail.com>
2020-04-21 22:41:03 +0100

    View Components

--- PieShop/Controllers/PieController.cs
+++ PieShop/Controllers/PieController.cs
@@ -34,24 +34,24 @@ namespace PieShop.Controllers
             ViewBag.CurrentCategory = "Cheese cakes";
 
             IEnumerable<Pie> pies;
-            //string currentCategory = string.Empty;
+            string currentCategory = string.Empty;
 
-            //if (string.IsNullOrEmpty(category))
-            //{
-            pies = _pieRepository.Pies.OrderBy(p => p.PieId);
-            //    currentCategory = "All pies";
-            //}
-            //else
-            //{
-            //    pies = _pieRepository.Pies.Where(p => p.Category.CategoryName == category)
-            //       .OrderBy(p => p.PieId);
-            //    currentCategory = _categoryRepository.Categories.FirstOrDefault(c => c.CategoryName == category).CategoryName;
-            //}
+            if (string.IsNullOrEmpty(category))
+            {
+                pies = _pieRepository.Pies.OrderBy(p => p.PieId);
+                currentCategory = "All pies";
+            }
+            else
+            {
+                pies = _pieRepository.Pies.Where(p => p.Category.CategoryName == category)
+                   .OrderBy(p => p.PieId);
+                currentCategory = _categoryRepository.Categories.FirstOrDefault(c => c.CategoryName == category).CategoryName;
+            }
 
             return View(new PiesListViewModel
             {
-                Pies = pies
-               // CurrentCategory = currentCategory
+                Pies = pies,
+                CurrentCategory = currentCategory
             });
         }
 
