57c7b9806ef2797c0efcce250f22a3485971c2bf
fabioono25 <fabioono25@gmail.com>
2020-04-23 22:57:08 +0100

    Add support to HEAD

--- CourseLibrary.API/Controllers/AuthorsController.cs
+++ CourseLibrary.API/Controllers/AuthorsController.cs
@@ -26,6 +26,7 @@ namespace CourseLibrary.API.Controllers
         }
 
         [HttpGet()]
+        [HttpHead]
         public ActionResult<IEnumerable<AuthorDto>> GetAuthors()
         {
             var authorsFromRepo = _courseLibraryRepository.GetAuthors();
