d37fe1fb5de741c4f1bc2b7d6c23ec737da161e0
fabioono25 <fabioono25@gmail.com>
2020-04-23 22:48:03 +0100

    adding Dtos + AutoMapper

--- CourseLibrary.API/Startup.cs
+++ CourseLibrary.API/Startup.cs
@@ -1,3 +1,4 @@
+using AutoMapper;
 using CourseLibrary.API.DbContexts;
 using CourseLibrary.API.Services;
 using Microsoft.AspNetCore.Builder;
@@ -8,6 +9,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using System;
 
 namespace CourseLibrary.API
 {
@@ -23,12 +25,14 @@ namespace CourseLibrary.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-           services.AddControllers(setup =>
-           {
-               setup.ReturnHttpNotAcceptable = true;
-               setup.OutputFormatters.Add(new XmlDataContractSerializerOutputFormatter())
-           }); //add controllers to DI 
-             
+            services.AddControllers(setup =>
+            {
+                setup.ReturnHttpNotAcceptable = true;
+                setup.OutputFormatters.Add(new XmlDataContractSerializerOutputFormatter());
+            }); //add controllers to DI 
+
+            services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
             services.AddScoped<ICourseLibraryRepository, CourseLibraryRepository>();
 
             services.AddDbContext<CourseLibraryContext>(options =>
