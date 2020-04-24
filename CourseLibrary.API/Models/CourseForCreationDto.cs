706069e524e054a4d68b9c1b6292bec1a9c78826
fabioono25 <fabioono25@gmail.com>
2020-04-24 20:39:54 +0100

    POST

--- CourseLibrary.API/Models/CourseForCreationDto.cs
+++ CourseLibrary.API/Models/CourseForCreationDto.cs
@@ -7,23 +7,24 @@ using System.Threading.Tasks;
 
 namespace CourseLibrary.API.Models
 {
-    [CourseTitleMustBeDifferentFromDescriptionAttribute(ErrorMessage = "Tittle different from description")]
-    public class CourseForCreationDto //: IValidatableObject
-    {
-        [Required(ErrorMessage = "Fill title")]
-        [MaxLength(100)]
-        public string Title { get; set; }
+    //[CourseTitleMustBeDifferentFromDescriptionAttribute(ErrorMessage = "Tittle different from description")]
+    //public class CourseForCreationDto //: IValidatableObject
+    //{
+    //    [Required(ErrorMessage = "Fill title")]
+    //    [MaxLength(100)]
+    //    public string Title { get; set; }
 
-        [MaxLength(1500)]
-        public string Description { get; set; }
+    //    [MaxLength(1500)]
+    //    public string Description { get; set; }
 
-        //public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
-        //{
-        //    if (Title == Description)
-        //    {
-        //        yield return new ValidationResult("The provided description should be different from the title.", new[] { "CourseForCreationDto" });
-        //    }
-        //}
-        //it's proposital not to have authorId... we avoid one more verification if this id = author id from URI
-    }
+    //    //public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+    //    //{
+    //    //    if (Title == Description)
+    //    //    {
+    //    //        yield return new ValidationResult("The provided description should be different from the title.", new[] { "CourseForCreationDto" });
+    //    //    }
+    //    //}
+    //    //it's proposital not to have authorId... we avoid one more verification if this id = author id from URI
+    //}
+    public class CourseForCreationDto : CourseForManipulationDto { }
 }
