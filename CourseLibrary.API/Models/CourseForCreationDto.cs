ae51144698e59ead4aa6ed1eb6da5ff5e511c050
fabioono25 <fabioono25@gmail.com>
2020-04-24 19:38:16 +0100

    add validations

--- CourseLibrary.API/Models/CourseForCreationDto.cs
+++ CourseLibrary.API/Models/CourseForCreationDto.cs
@@ -1,15 +1,29 @@
-﻿using System;
+﻿using CourseLibrary.API.ValidationAttributes;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace CourseLibrary.API.Models
 {
-    public class CourseForCreationDto
+    [CourseTitleMustBeDifferentFromDescriptionAttribute(ErrorMessage = "Tittle different from description")]
+    public class CourseForCreationDto //: IValidatableObject
     {
+        [Required(ErrorMessage = "Fill title")]
+        [MaxLength(100)]
         public string Title { get; set; }
 
+        [MaxLength(1500)]
         public string Description { get; set; }
+
+        //public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        //{
+        //    if (Title == Description)
+        //    {
+        //        yield return new ValidationResult("The provided description should be different from the title.", new[] { "CourseForCreationDto" });
+        //    }
+        //}
         //it's proposital not to have authorId... we avoid one more verification if this id = author id from URI
     }
 }
