ab29d09471b4903f0c7ea230440eb2d2cd489b61
fabioono25 <fabioono25@gmail.com>
2020-04-25 10:26:05 +0100

    add entities and migrations

--- Books.Api/Startup.cs
+++ Books.Api/Startup.cs
@@ -1,15 +1,10 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Books.Api.Contexts;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace Books.Api
 {
@@ -25,6 +20,9 @@ namespace Books.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            var connectionString = Configuration.GetConnectionString("BooksDBConnectionString");
+            services.AddDbContext<BooksContext>(o => o.UseSqlServer(connectionString));
+
             services.AddControllers();
         }
 
