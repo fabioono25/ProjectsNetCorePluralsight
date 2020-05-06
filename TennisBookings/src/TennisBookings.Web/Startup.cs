51b159bb42dcce59b9d7a5920857356e8f919587
fabioono25 <fabioono25@gmail.com>
2020-05-06 22:43:38 +0100

    Add project to work with DI

---
+++ TennisBookings/src/TennisBookings.Web/Startup.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using TennisBookings.Web.Services;
+ using Microsoft.Extensions.Hosting;
+
+namespace TennisBookings.Web
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddTransient<IWeatherForecaster, AmazingWeatherForecaster>();
+
+            services.AddControllersWithViews(mvc => mvc.EnableEndpointRouting = false);
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseFileServer();
+
+            app.UseMvc();
+        }
+    }
+}
