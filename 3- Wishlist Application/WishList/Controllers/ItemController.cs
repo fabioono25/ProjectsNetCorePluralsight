8144bb7da4da53f6ca4899cede01d9c5a7a63f48
Fabio Ono Tavoloni <fabioono25@gmail.com>
2020-07-25 00:33:59 +0100

    Add Wishlist project

---
+++ 3- Wishlist Application/WishList/Controllers/ItemController.cs
@@ -0,0 +1,48 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using WishList.Data;
+using WishList.Models;
+
+namespace WishList.Controllers
+{
+    public class ItemController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ItemController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var model = _context.Items.ToList();
+
+            return View("Index", model);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View("Create");
+        }
+
+        [HttpPost]
+        public IActionResult Create(Item item)
+        {
+            _context.Items.Add(item);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public IActionResult Delete(int id)
+        {
+            var model = _context.Items.FirstOrDefault(i => i.Id == id);
+
+            _context.Remove(model);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
