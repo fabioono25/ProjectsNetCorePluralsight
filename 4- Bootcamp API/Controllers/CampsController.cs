8170f84f6246ef14ea35b2465a47d4e9d3a6c19b
Fabio Ono Tavoloni <fabioono25@gmail.com>
2020-08-12 14:06:17 +0100

    adding a basic API + support to AutoMapper

---
+++ 4- Bootcamp API/Controllers/CampsController.cs
@@ -0,0 +1,42 @@
+﻿using AutoMapper;
+using CoreCodeCamp.Data;
+using CoreCodeCamp.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace CoreCodeCamp.Controllers
+{
+    [Route("api/[controller]")]
+    public class CampsController: ControllerBase
+    {
+        private readonly ICampRepository _repository;
+        private readonly IMapper _mapper;
+
+        public CampsController(ICampRepository repository, IMapper mapper)
+        {
+            _repository = repository;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<CampModel[]>> GetCamps()
+        {
+            try
+            {
+                var results = await _repository.GetAllCampsAsync();
+
+                //CampModel[] models = _mapper.Map<CampModel[]>(results);
+
+                //return Ok(models);
+                return _mapper.Map<CampModel[]>(results);
+            }
+            catch (System.Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Database Failure");
+            }
+
+            //return Ok(new[] { "Hello", "From", "Pluralsight" });
+        }
+    }
+}
