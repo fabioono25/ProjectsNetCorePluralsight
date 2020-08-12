f4b67f3d758f97c6ba2fd219adba3df4a704ee48
Fabio Ono Tavoloni <fabioono25@gmail.com>
2020-08-12 16:47:11 +0100

    Implementing association

---
+++ 4- Bootcamp API/Controllers/TaksController.cs
@@ -0,0 +1,165 @@
+﻿using AutoMapper;
+using CoreCodeCamp.Data;
+using CoreCodeCamp.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.EntityFrameworkCore.Internal;
+using System;
+using System.Threading.Tasks;
+
+namespace CoreCodeCamp.Controllers
+{
+    [Route("api/camps/{moniker}/taks")]
+    [ApiController]
+    public class TaksController : ControllerBase
+    {
+        private readonly ICampRepository _repository;
+        private readonly IMapper _mapper;
+        private readonly LinkGenerator _linkGenerator;
+
+        public TaksController(ICampRepository repository, IMapper mapper, LinkGenerator linkGenerator)
+        {
+            _repository = repository;
+            _mapper = mapper;
+            _linkGenerator = linkGenerator;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<TalkModel[]>> Get(string moniker)
+        {
+            try
+            {
+                var talks = await _repository.GetTalksByMonikerAsync(moniker);
+
+                return _mapper.Map<TalkModel[]>(talks);
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Database Failure");
+            }
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult<TalkModel>> Get(string moniker, int id)
+        {
+            try
+            {
+                var talk = await _repository.GetTalkByMonikerAsync(moniker, id);
+
+                if (talk == null) return NotFound($"Could not found talk.");
+
+                return _mapper.Map<TalkModel>(talk);
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Database Failure");
+            }
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<TalkModel>> Post(string moniker, TalkModel model)
+        {
+            try
+            {
+                var camp = await _repository.GetCampAsync(moniker);
+
+                if (camp == null)
+                {
+                    return BadRequest("Camp does not exist");
+                }
+
+                var talk = _mapper.Map<Talk>(model);
+                talk.Camp = camp;
+                _repository.Add(talk);
+
+                if (model.Speaker == null) return BadRequest("Speaker ID is required");
+                var speaker = await _repository.GetSpeakerAsync(model.Speaker.SpeakerId);
+                if (speaker == null) return BadRequest("Speaker could not be found");
+                talk.Speaker = speaker;
+
+                if (await _repository.SaveChangesAsync())
+                {
+                    var url = _linkGenerator.GetPathByAction(HttpContext, "Get",
+                        values: new { moniker, id = talk.TalkId });
+
+                    return Created(url, _mapper.Map<TalkModel>(talk));
+                }
+                else
+                {
+                    return BadRequest("Failed do save new talk");
+                }
+            }
+            catch
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Database Failure");
+            }
+        }
+
+        [HttpPut("{id: int}")]
+        public async Task<ActionResult<TalkModel>> Put(string moniker, int id, TalkModel model)
+        {
+            try
+            {
+                var talk = await _repository.GetTalkByMonikerAsync(moniker, id, true);
+                
+                if (talk == null)
+                {
+                    return BadRequest("Talk does not exist");
+                }
+
+                if (model.Speaker != null)
+                {
+                    var speaker = await _repository.GetSpeakerAsync(model.Speaker.SpeakerId);
+
+                    if (speaker != null)
+                    {
+                        talk.Speaker = speaker;
+                    }
+                }
+
+                _mapper.Map(model, talk);
+
+                if (await _repository.SaveChangesAsync())
+                {
+                    return _mapper.Map<TalkModel>(talk);
+                }else
+                {
+                    return BadRequest("Failed do update database");
+                }
+            }
+            catch
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Database Failure");
+            }
+        }
+
+        [HttpDelete("{id:int}")]
+        public async Task<ActionResult<CampModel>> Delete(string moniker, int id)
+        {
+            try
+            {
+                var talk = await _repository.GetTalkByMonikerAsync(moniker, id);
+
+                if (talk == null)
+                {
+                    return NotFound($"Could not found talk.");
+                }
+
+                _repository.Delete(talk);
+
+                if (await _repository.SaveChangesAsync())
+                {
+                    return Ok();
+                }else
+                {
+                    return BadRequest($"Failed to delete talk.");
+                }
+            }
+            catch
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Database Failure");
+            }
+        }
+    }
+}
--- 4- Bootcamp API/Data/CampProfile.cs
+++ 4- Bootcamp API/Data/CampProfile.cs
@@ -10,6 +10,13 @@ namespace CoreCodeCamp.Data
             this.CreateMap<Camp, CampModel>()
                 .ForMember(c => c.Venue, o => o.MapFrom(m => m.Location.VenueName));
 
+            this.CreateMap<Talk, TalkModel>()
+                .ReverseMap()
+                .ForMember(t => t.Camp, opt => opt.Ignore()) //TalkModel -> Talk (ignore)
+                .ForMember(t => t.Speaker, opt => opt.Ignore());
+
+            this.CreateMap<Speaker, SpeakerModel>()
+                .ReverseMap();
         }
     }
 }
