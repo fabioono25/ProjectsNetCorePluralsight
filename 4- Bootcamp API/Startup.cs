6afda3fef79ed98b92de6c82cf39fe747ad4e1df
Fabio Ono Tavoloni <fabioono25@gmail.com>
2020-08-12 12:20:07 +0100

    API Basics - bootstrap of the project

---
+++ 4- Bootcamp API/Startup.cs
@@ -0,0 +1,30 @@
+﻿using CoreCodeCamp.Data;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CoreCodeCamp
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<CampContext>();
+            services.AddScoped<ICampRepository, CampRepository>();
+
+            services.AddMvc()
+              .SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc();
+        }
+    }
+}
