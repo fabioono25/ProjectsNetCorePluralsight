5077fde9b13f52740757c7eeafca76bc45116ad3
Fabio Ono Tavoloni <fabioono25@gmail.com>
2020-10-12 16:35:18 +0100

    Adding a test file

---
+++ 11-GenDocumentationWithSwagger/PostmanTestExample.js
@@ -0,0 +1,9 @@
+﻿pm.test("Status code is 200", function () {
+    pm.response.to.have.status(200);
+    console.log(pm.response);
+});
+
+pm.test("Author returned must match requested author", function () {
+    var jsonData = pm.response.json();
+    pm.expect(jsonData.id).to.equal("2902b665-1190-4c70-9915-b9c2d7680450")
+});
