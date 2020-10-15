3cba377ea12e49b9ce6dd143b62fb8e45d491ea5
Fabio Ono Tavoloni <fabioono25@gmail.com>
2020-10-15 17:10:16 +0100

    bootstrap for async API

--- 11-GenDocumentationWithSwagger/PostmanTestExample.js
+++ 11-GenDocumentationWithSwagger/PostmanTestExample.js
@@ -7,3 +7,17 @@ pm.test("Author returned must match requested author", function () {
     var jsonData = pm.response.json();
     pm.expect(jsonData.id).to.equal("2902b665-1190-4c70-9915-b9c2d7680450")
 });
+
+// BEGIN - we can add in the pre-request script
+var responseTimeTest = () => pm.test("response time bellow 300ms", function () {
+    pm.expect(pm.response.responseTime).to.be.below(300)
+});
+
+// pm.environment
+pm.globals.set("responseTimeTest", responseTimeTest.toString());
+// END - we can add in the pre-request script 
+
+
+// this test can be used in all other APIs
+eval(pm.globals.get("responseTimeTest"))();
+
